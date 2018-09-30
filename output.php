e20722cb61617ff8cc149481e8d543542fa9e3a1
B MANISH <manishchoradia1@gmail.com>
2018-09-30 20:37:29 +0530

    Update output.php

--- output.php
+++ output.php
@@ -1,10 +1,17 @@
 <html>
 <head>
 	<title>Output</title>
+	<style type="text/css">
+		div {
+			position:relative;
+			left:600px;
+		}
+	</style>
 </head>
 <body bgcolor="orange">
 <h2 align="center"> Student Projects Evaluated As : 
 <br><br>
+<div> 
 <?php
 $handle = fopen('file3.txt','r');
 $n=fgets($handle);
@@ -27,6 +34,7 @@ echo "</table>";
 fclose($handle);
 
 ?>
+</div>
 </h2>
 </body>
 </html>
